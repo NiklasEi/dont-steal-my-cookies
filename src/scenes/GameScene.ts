a4cb66923a231cb6c416c43931e3be557549fd9e
Niklas Eicker <git@nikl.me>
2020-12-06 14:39:00 +0100

    Tween a hand to the plate

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,20 +1,39 @@
 import * as Phaser from 'phaser';
-import {assetKeys, scenes} from "../utils/constants";
+import {assetKeys, dimensions, scenes} from "../utils/constants";
+import Vector2 = Phaser.Math.Vector2;
 
 export class GameScene extends Phaser.Scene {
+  private readonly handOffset = new Vector2(100, 0);
+  private readonly plate = new Vector2(dimensions.width / 2, dimensions.height / 2);
+  private hand?: Phaser.GameObjects.Image;
+
   constructor() {
     super(scenes.gameScene);
   }
 
   create() {
     this.scene.run(scenes.gameHud);
-    this.add.image(
-        this.game.renderer.width / 2,
-        this.game.renderer.height / 2,
+    const background = this.add.image(
+        dimensions.width / 2,
+        dimensions.height / 2,
         assetKeys.table)
+    background.scale = 0.5;
   }
 
   update() {
-    // updating game scene
+    if (this.hand === undefined) {
+      const startPoint = new Vector2(0, 0);
+      const dist = this.plate.subtract(startPoint.add(this.handOffset));
+      this.hand = this.add.image(
+          0,
+          0,
+          assetKeys.hand)
+      this.add.tween({targets: this.hand,
+        y: dist.y,
+        x: dist.x,
+        duration: 2000,
+        // eslint-disable-next-line no-console
+        onComplete: () => console.log("done")});
+    }
   }
 }
